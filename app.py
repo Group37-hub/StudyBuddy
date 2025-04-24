1eb674424854c2d32ee5ad99a7e109b277196b57
Amal Habane <amalhabane1@gmail.com>
2025-04-24 18:48:47 +0100

    update app.py

--- app.py
+++ app.py
@@ -10,26 +10,27 @@ user = User(
     StudyPreferences("1st", "Computer Science", "Male", "Morning", "Library")
 )
 
+
 @app.route('/')
 def home():
     return render_template('home.html')
+
 
 @app.route('/edit_preferences', methods=['GET', 'POST'])
 def edit_preferences():
     if request.method == 'POST':
 
         user.update_study_preferences(
-            year      = request.form['year'],
-            subject   = request.form['subject'],
-            gender    = request.form['gender'],
-            time      = request.form['time'],
-            location  = request.form['location']
+            year=request.form['year'],
+            subject=request.form['subject'],
+            gender=request.form['gender'],
+            time=request.form['time'],
+            location=request.form['location']
         )
         return redirect(url_for('home'))
 
-
     return render_template('profile.html', user=user)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
-
