d16a4a837a934bc0310ad1a82bc6a5ef8ab1ec9f
izzynorwood <izzynorwoodmk@gmail.com>
2025-04-30 15:32:24 +0100

    Create room.py

---
+++ app/models/room.py
@@ -0,0 +1,6 @@
+class Room(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    room_name = db.Column(db.String(100), unique=True, nullable=False)
+    building = db.Column(db.String(200), nullable=True)
+    capacity = db.Column(db.Integer, nullable=False)
+    bookings = db.relationship('Booking', backref='room', lazy=True)
