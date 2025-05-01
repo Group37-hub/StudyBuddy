377850aa6f0b65e71b9fc084e3e55b056f59e465
izzynorwood <izzynorwoodmk@gmail.com>
2025-05-01 11:03:39 +0100

    Update booking.py

--- app/models/booking.py
+++ app/models/booking.py
@@ -12,13 +12,31 @@ class Booking(db.Model):
     booking_end = db.Column(db.DateTime, nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.now)
     status = db.Column(db.String(20), nullable=False, default='pending')
+    formatted_meeting_date = db.Column(db.String(20))
 
+    # Finds when the booking is over
     def get_booking_end(self):
         booking_end = datetime.combine(self.week_beginning, datetime.min.time()) + timedelta(days=self.day,hours=self.hour+1)
         return booking_end
 
+    # Checks if the booking is past
     def is_past(self):
         return self.booking_end < datetime.now()
+
+    # Formats the meeting date
+    def format_meeting_date(self):
+        meeting_date = self.week_beginning + timedelta(days=self.day)
+        day_name = meeting_date.strftime('%A')
+        month_name = meeting_date.strftime('%B')
+        day_with_suffix = self.get_ordinal()
+        return f"{day_name} the {day_with_suffix} of {month_name}"
+
+    def get_ordinal(self):
+        if 11 <= self.day % 100 <= 13:
+            suffix = 'th'
+        else:
+            suffix = {1: 'st', 2: 'nd', 3: 'rd'}.get(self.day % 10, 'th')
+        return f"{self.day}{suffix}"
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
@@ -28,3 +46,4 @@ class Booking(db.Model):
             self.week_beginning = monday.date()
 
         self.booking_end = self.get_booking_end()
+        self.formatted_meeting_date = self.format_meeting_date()
